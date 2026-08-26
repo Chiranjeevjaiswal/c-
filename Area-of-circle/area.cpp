5f0a902b639489bcb52cbbf791ae976fe40dbec4
Chiranjeev Jaiswal <chiranjeev.25scs1003002575@iilm.edu>
2026-08-26 20:43:19 +0530

    C++ program to calculate area of a circle
    this is my practice c++ problem ,given by the university , this to find area of a circle

---
+++ Area-of-circle/area.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r, area;
+    cout<<"what is radius of circle:";
+    cin>>r;
+    area= 3.14*r*r;
+    cout<<"the area of circle:"<<area;
+    return 0;
+} 
