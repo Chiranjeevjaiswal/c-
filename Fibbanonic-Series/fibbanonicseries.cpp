534d73bfd9768f5e683a1552686c32672210fd8d
Chiranjeev Jaiswal <chiranjeev.25scs1003002575@iilm.edu>
2026-08-27 15:58:07 +0530

    Fibonacci series in C++

---
+++ Fibbanonic-Series/fibbanonicseries.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+    int n,a=0,b=1,c;
+    cout<<"enter the no. of term:";
+    cin>>n;
+    cout<<"fibbanonic series:";
+    for(int i=1;i<=n;i++)
+    {
+        c=a+b;
+        a=b;
+        b=c;
+        cout<<c<<" ";
+    }
+    return 0;
+}
